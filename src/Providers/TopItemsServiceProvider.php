e48c07e6c90956245b4febd316539905de43bf39
farbfinal <info@farbfinal.de>
2017-04-10 13:26:01 +0200

    Display Top Items

---
+++ src/Providers/TopItemsServiceProvider.php
@@ -0,0 +1,8 @@
+<?php
+namespace TopItems\Providers;
+use Plenty\Plugin\ServiceProvider;
+class TopItemsServiceProvider extends ServiceProvider {
+    public function register() {
+        $this->getApplication()->register(TopItemsRouteServiceProvider::class);
+    }
+}
